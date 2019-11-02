c572e8ab94846ebc55af7675f4c150bc45e8c2ea
udaybansal19 <udaybansal19@gmail.com>
2019-11-02 23:38:21 +0530

    post request finally works from the server

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 var routes = require("express").Router();
 
-routes.get('/', function(req, res){
-    res.json("hello");
- });
-routes.post('/', function(req,res){
-    var xhr = new XMLHttpRequest();
-    var url = "https://api.jdoodle.com/v1/execute";
-
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function sub() {
-    if (xhr.statusCode === 200) {
-        var json = JSON.parse(xhr.responseText);
-        console.log(json.output);
-    }
-    };
-    var code = document.getElementById("text-area").value;
-    console.log(code);
-    var data = JSON.stringify({
-    "script" : "print(1234)",
-    "language" : "python2",
-    "versionIndex" : "0",
-    "clientId" : "55b38b73c23c3a81677bd5d26c97a403",
-    "clientSecret" : "6d891bf44cbf04fb235a3ef62e8a3a7f10f842e64e9ccd27fd435c85891e6e73"
-    });
-    xhr.send(data);
-});
 
 
 
--- server.js
+++ server.js
@@ -1,17 +1,58 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 const path = require("path");
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var routes = express.Router();
 
+var url = "https://api.jdoodle.com/v1/execute";
+
+ var obj = {
+    "script" : "print(1234)",
+    "language" : "python2",
+    "versionIndex" : "0",
+    "clientId" : "55b38b73c23c3a81677bd5d26c97a403",
+    "clientSecret" : "6d891bf44cbf04fb235a3ef62e8a3a7f10f842e64e9ccd27fd435c85891e6e73"
+    };
+    //console.log(obj.script);
+request.post({
+    url: url,
+    headers: {'content-type' : 'application/json'},
+    body: obj,
+    json: true,
+  },
+function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log("FLAG1");
+        console.log(body);
+        console.log("FLAG2");
+    }else{
+       console.log(body);
+     //console.log(response);
+        console.log("FAILED");
+    }
+}
+);
+
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
 app.post('/code', function(req,res){
     var xhr = new XMLHttpRequest();
-    var url = "https://api.jdoodle.com/v1/execute";
+
+
+
+    var data = JSON.stringify({
+        "script" : "print(1234)",
+        "language" : "python2",
+        "versionIndex" : "0",
+        "clientId" : "55b38b73c23c3a81677bd5d26c97a403",
+        "clientSecret" : "6d891bf44cbf04fb235a3ef62e8a3a7f10f842e64e9ccd27fd435c85891e6e73"
+        });
+    
 
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
@@ -20,19 +61,13 @@ app.post('/code', function(req,res){
         var json = JSON.parse(xhr.responseText);
         res.json(json);
     }else{
-        console.log("--Flag1--");
-        console.log(xhr.statusCode);
-        console.log("--Flag--");
+        //console.log("--Flag1--");
+        //console.log(xhr);
+        //console.log("--Flag--");
         res.json({message: "not found"});
     }
     };
-    var data = JSON.stringify({
-    "script" : "print(1234)",
-    "language" : "python2",
-    "versionIndex" : "0",
-    "clientId" : "55b38b73c23c3a81677bd5d26c97a403",
-    "clientSecret" : "6d891bf44cbf04fb235a3ef62e8a3a7f10f842e64e9ccd27fd435c85891e6e73"
-    });
+    
     xhr.send(data);
 });
 
