3cdb87d4d8229926551914df629f8c41aa845cc0
udaybansal19 <udaybansal19@gmail.com>
2019-11-02 23:56:26 +0530

    Edits to the /code api

--- server.js
+++ server.js
@@ -2,38 +2,9 @@ var express = require('express');
 var app = express();
 var request = require('request');
 const path = require("path");
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var routes = express.Router();
 
-var url = "https://api.jdoodle.com/v1/execute";
-
- var obj = {
-    "script" : "print(1234)",
-    "language" : "python2",
-    "versionIndex" : "0",
-    "clientId" : "55b38b73c23c3a81677bd5d26c97a403",
-    "clientSecret" : "6d891bf44cbf04fb235a3ef62e8a3a7f10f842e64e9ccd27fd435c85891e6e73"
-    };
-    //console.log(obj.script);
-request.post({
-    url: url,
-    headers: {'content-type' : 'application/json'},
-    body: obj,
-    json: true,
-  },
-function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log("FLAG1");
-        console.log(body);
-        console.log("FLAG2");
-    }else{
-       console.log(body);
-     //console.log(response);
-        console.log("FAILED");
-    }
-}
-);
 
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -41,34 +12,36 @@ app.get('/', function(req,res) {
 
 
 app.post('/code', function(req,res){
-    var xhr = new XMLHttpRequest();
+    console.log(req.body);
+    var url = "https://api.jdoodle.com/v1/execute";
 
+    var obj = {
+       "script" : "print(1234)",
+       "language" : "python2",
+       "versionIndex" : "0",
+       "clientId" : "55b38b73c23c3a81677bd5d26c97a403",
+       "clientSecret" : "6d891bf44cbf04fb235a3ef62e8a3a7f10f842e64e9ccd27fd435c85891e6e73"
+       };
+   request.post({
+       url: url,
+       headers: {'content-type' : 'application/json'},
+       body: obj,
+       json: true,
+     },
+   function (error, response, body) {
+       if (!error && response.statusCode == 200) {
+        res.statusCode = 200;    
+        res.json(body);
 
-
-    var data = JSON.stringify({
-        "script" : "print(1234)",
-        "language" : "python2",
-        "versionIndex" : "0",
-        "clientId" : "55b38b73c23c3a81677bd5d26c97a403",
-        "clientSecret" : "6d891bf44cbf04fb235a3ef62e8a3a7f10f842e64e9ccd27fd435c85891e6e73"
-        });
-    
-
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function sub() {
-    if (xhr.statusCode === 200) {
-        var json = JSON.parse(xhr.responseText);
-        res.json(json);
-    }else{
-        //console.log("--Flag1--");
-        //console.log(xhr);
-        //console.log("--Flag--");
+           console.log(body);
+       }else{
         res.json({message: "not found"});
-    }
-    };
-    
-    xhr.send(data);
+          console.log(body);
+        //console.log(response);
+           console.log("FAILED");
+       }
+   }
+   );
 });
 
 
@@ -76,3 +49,5 @@ app.post('/code', function(req,res){
 var server = app.listen(8081, () => {
  console.log("Page is running on: " + server.address().port);
 }); 
+
+
