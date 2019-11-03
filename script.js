696bc01f40611d03fe4db3169cb03195372fb31f
udaybansal19 <udaybansal19@gmail.com>
2019-11-03 22:10:31 +0530

    -

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var routes = require("express").Router();
 
+document.getElementById("submit").addEventListener("click",postCode());
 
+function postCode(){
 
+    var xhttp = new XMLHttpRequest();
 
+    xhttp.open("POST", "http://localhost:8081/code", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    var script = document.getElementById("code").value;
+    var code = {
+        "script": script
+    }
+    xhttp.send(code);
+    xhttp.onreadystatechange = function() {
+        if(this.)
+    }
+}
